ec7ef04caf130cde97c9a1f3045a156a0a60475f
abdul-kabugu <kabuguabdul2@gmail.com>
2024-04-27 01:40:46 +0300

    Feat : added warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 'use client'
 import { GeistSans } from "geist/font/sans";
+import { useState } from "react";
 import "./globals.css";
 import Index from "@/components/Topnavbar/Index";
 import { ProfileContextProvider } from "@/components/ProfileContext";
 import { SlideProvider } from "@/components/QuestStepsContext";
 // In app directory
 import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
+import { MdClose } from "react-icons/md";
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
@@ -21,6 +23,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+
+   const [showWarning, setshowWarning] = useState(true)
+
+  
   return (
     <html lang="en" className={GeistSans.className}>
       <body className="bg-gray-950 text-gray-300">
@@ -31,6 +37,12 @@ export default function RootLayout({
           shallowRouting
         />
         <main className="min-h-screen ">
+           {showWarning  &&
+           <div className="w-full sticky top-0 h-6 bg-red-600 animate-pulse max-w-6xl mx-auto px-6 flex items-center justify-between">
+             <p className="text-white text-center ">still in developement things  might break</p>
+              <MdClose  className="text-white w-5 h-5 cursor-pointer" onClick={() => setshowWarning(! showWarning)} />
+           </div>
+}
           <ProfileContextProvider>
         
           <Index   />
